a8fb8a29e7fa3a35923e4bc2fe75c1dcee9d423f
Bulgarest <elitethehuman@gmail.com>
2025-05-14 01:44:02 +0300

    final version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { FooterBarComponent } from './footer-bar/footer-bar.component';
 import { MainComponentComponent } from './main-component/main-component.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +20,13 @@ import { MainComponentComponent } from './main-component/main-component.componen
     MainComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
